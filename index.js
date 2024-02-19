8cd1fa81f6a9a8a45d5b07fee7f143dff27b57bb
Spencer <spinsab@gmail.com>
2024-02-19 14:05:14 -0500

    conditions in JS

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result
+  if (feet <= 400) {
+     result = 'This one is on me!';
+  } else if (feet <=2000) {
+    result = 'That will be twenty bucks.';
+  } else if (feet <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else {
+    result = 'No can do.';
+    }
+
+    return result;
+
+  }
+
+
+
+
+function ternaryCheckCity(city){
+  
+  let cityStatus
+  cityStatus = city === `NYC` ? "Ok, sounds good." :"No go.";
+
+  return cityStatus;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(amount){
+  
+  let tipResponse;
+  
+  switch (amount) {
+    case amount = 'generous':
+      tipResponse = `Thank you so much.`;
+      break;
+    case amount = 'not as generous':
+      tipResponse = `Thank you.`;
+      break;
+    default:
+      tipResponse = `Bye.`;
+      break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
+
+  return tipResponse;
 }
